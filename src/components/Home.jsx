5ee5b7bdd898ce66532d4e0e094cba5e8570e1dc
ChanHengLucas <91720922+ChanHengLucas@users.noreply.github.com>
2025-05-13 09:02:17 -0400

    e

---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import './Home.css';
+
+// This would become the main menu for the game
+// Remember to implement multiplayer settings
+
+function Home() {
+    return (
+        <div>
+            <h1>Welcome to the Home Page</h1>
+            <p>This is a simple home page component.</p>
+        </div>
+    );
+}
+
+export default Home;
